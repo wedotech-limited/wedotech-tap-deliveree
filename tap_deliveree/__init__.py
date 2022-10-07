2f9c49a409f27eecaee7c94c5206ebdd945902bf
Ashley G Ramdass <ashley@wedotech.co>
2022-10-07 08:01:16 +0000

    feat: support for different report resource path

--- tap_deliveree/__init__.py
+++ tap_deliveree/__init__.py
@@ -1,47 +1,51 @@
-import singer 
+import singer
 from singer.catalog import Catalog, write_catalog
 from .report_stream import ReportStream
 
 LOGGER = singer.get_logger()
 
-REQUIRED_CONFIG_KEYS = ["base_url", "resource", "reports", "api_key"]
+REQUIRED_CONFIG_KEYS = ["base_url", "reports", "api_key"]
+
 
 def discover(reports):
-  LOGGER.info("Discovery start")
-  catalog_entries = []
-  for report in reports:
-    report_stream = ReportStream(report)
-    catalog_entries.append(report_stream.generate_catalog_entry())
-  catalog = Catalog(catalog_entries)
-  write_catalog(catalog)
-  LOGGER.info("Discovery complete")
+    LOGGER.info("Discovery start")
+    catalog_entries = []
+    for report in reports:
+        report_stream = ReportStream(report)
+        catalog_entries.append(report_stream.generate_catalog_entry())
+    catalog = Catalog(catalog_entries)
+    write_catalog(catalog)
+    LOGGER.info("Discovery complete")
+
 
 def generate_catalog(reports):
-  catalog_entries = []
-  for report in reports:
-    report_stream = ReportStream(report)
-    catalog_entries.append(report_stream.generate_catalog_entry())
-  return Catalog(catalog_entries)
+    catalog_entries = []
+    for report in reports:
+        report_stream = ReportStream(report)
+        catalog_entries.append(report_stream.generate_catalog_entry())
+    return Catalog(catalog_entries)
+
 
 def get_report_stream(reports, tap_stream_id):
-  report = next((report for report in reports if report["name"] == tap_stream_id), None)
-  if report is None:
-    return None
-  return ReportStream(report)
+    report = next((report for report in reports if report["name"] == tap_stream_id), None)
+    if report is None:
+        return None
+    return ReportStream(report)
 
 
 def sync(config, state, catalog):
-  LOGGER.info("Sync start")
-  selected_streams = catalog.get_selected_streams(state)
-  reports = config["reports"]
-  for stream in selected_streams:
-    report_stream = get_report_stream(reports, stream.tap_stream_id)
-    if report_stream is None:
-      LOGGER.error(f"Report not found: {stream.tap_stream_id}")
-      continue
-    report_stream.write_schema()
-    report_stream.sync(config, state)
-  LOGGER.info("Sync complete")
+    LOGGER.info("Sync start")
+    selected_streams = catalog.get_selected_streams(state)
+    reports = config["reports"]
+    for stream in selected_streams:
+        report_stream = get_report_stream(reports, stream.tap_stream_id)
+        if report_stream is None:
+            LOGGER.error(f"Report not found: {stream.tap_stream_id}")
+            continue
+        report_stream.write_schema()
+        report_stream.sync(config, state)
+    LOGGER.info("Sync complete")
+
 
 def main():
     args = singer.utils.parse_args(required_config_keys=REQUIRED_CONFIG_KEYS)
@@ -49,13 +53,14 @@ def main():
     reports = config["reports"]
 
     if args.discover:
-      discover(reports)
+        discover(reports)
     else:
-      if args.catalog:
-        catalog = args.catalog
-      else:
-        catalog = generate_catalog(reports)
-      sync(config, args.state, catalog)
+        if args.catalog:
+            catalog = args.catalog
+        else:
+            catalog = generate_catalog(reports)
+        sync(config, args.state, catalog)
+
 
 if __name__ == "__main__":
     main()
--- tap_deliveree/report_stream.py
+++ tap_deliveree/report_stream.py
@@ -1,4 +1,4 @@
-import singer 
+import singer
 from singer.catalog import CatalogEntry
 from singer.schema import Schema
 from singer.metrics import http_request_timer
@@ -8,93 +8,91 @@ from .utils import parse_parameters
 
 LOGGER = singer.get_logger()
 
+
 class ReportStream:
-  def __init__(self, report) -> None:
-    self.report = report
 
-  def get_key_properties(self):
-    return self.report["key_properties"]
+    def __init__(self, report) -> None:
+        self.report = report
 
-  def generate_schema(self):
-    schema = dict()
-    schema["title"] = self.report["name"]
-    schema["type"] = ["null", "object"]
-    properties = dict()
-    schema["properties"] = properties
-    for property in self.report["properties"]:
-      types = [property["type"]]
-      if property.get("nullable") is True:
-        types.append("null")
+    def get_key_properties(self):
+        return self.report["key_properties"]
 
-      properties[property["name"]] = {"type": types}
+    def generate_schema(self):
+        schema = dict()
+        schema["title"] = self.report["name"]
+        schema["type"] = ["null", "object"]
+        properties = dict()
+        schema["properties"] = properties
+        for property in self.report["properties"]:
+            types = [property["type"]]
+            if property.get("nullable") is True:
+                types.append("null")
 
-    # Add data property
-    properties["data"] = {
-      "type": ["null", "object"],
-    }
+            properties[property["name"]] = {"type": types}
 
-    return schema
+        return schema
 
-  def generate_catalog_entry(self):
-    stream = self.report["name"]
-    schema = self.generate_schema()
-    key_properties = self.report["key_properties"]
-    metadata = singer.metadata.get_standard_metadata(
-        schema=schema,
-        key_properties=key_properties,
-        valid_replication_keys=key_properties,
-        replication_method="INCREMENTAL"
-      )
+    def generate_catalog_entry(self):
+        stream = self.report["name"]
+        schema = self.generate_schema()
+        key_properties = self.report["key_properties"]
+        metadata = singer.metadata.get_standard_metadata(schema=schema,
+                                                         key_properties=key_properties,
+                                                         valid_replication_keys=key_properties,
+                                                         replication_method="INCREMENTAL")
 
-    return CatalogEntry(tap_stream_id=stream, stream=stream, schema=Schema.from_dict(schema), metadata=metadata)
+        return CatalogEntry(tap_stream_id=stream,
+                            stream=stream,
+                            schema=Schema.from_dict(schema),
+                            metadata=metadata)
 
-  def write_schema(self):
-    schema = self.generate_schema()
-    key_properties = self.report["key_properties"]
-    return singer.write_schema(stream_name=self.report["name"], schema=schema, key_properties=key_properties)
+    def write_schema(self):
+        schema = self.generate_schema()
+        key_properties = self.report["key_properties"]
+        return singer.write_schema(stream_name=self.report["name"],
+                                   schema=schema,
+                                   key_properties=key_properties)
 
-  @backoff.on_exception(backoff.expo, Exception, max_tries=5, factor=2)
-  def get_data(self, config):
-    url = f"{config['base_url']}/{config['resource']}/{self.report['name']}"
-    LOGGER.debug(f"Sending request to URL: {url}")
-    headers = {
-      "Content-Type": "application/json",
-      "Authorization": f"Basic {config['api_key']}"
-    }
+    @backoff.on_exception(backoff.expo, Exception, max_tries=5, factor=2)
+    def get_data(self, config):
+        url = f"{config['base_url']}/{self.report['resource']}/{self.report['name']}"
+        LOGGER.debug(f"Sending request to URL: {url}")
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Basic {config['api_key']}"
+        }
 
-    parameters = parse_parameters(self.report["parameters"])
-    response = requests.get(url, headers=headers, params=parameters)
-    response.raise_for_status()
+        parameters = parse_parameters(self.report["parameters"])
+        response = requests.get(url, headers=headers, params=parameters)
+        response.raise_for_status()
 
-    return response.json(), parameters
+        return response.json(), parameters
 
-  def get_records(self, config):
-    
-    with http_request_timer(self.report["name"]):
-      data, parameters = self.get_data(config)
-      
-    records = []
-    for entry in data:
-      record = dict()
-      for property in self.report["properties"]:
-        record[property["name"]] = entry[property["name"]]
-      record["data"] = entry
-      records.append(record)
-    return records, parameters
+    def get_records(self, config):
 
-  def sync(self, config, state):
-    # time_extracted = singer.utils.now()
-    records, parameters = self.get_records(config)
-    singer.write_records(self.report["name"], records)
-    self._write_state(state, parameters)
+        with http_request_timer(self.report["name"]):
+            data, parameters = self.get_data(config)
 
-  def _write_state(self, state, parameters):
-    report_state = state.get(self.report["name"], [])
-    state[self.report["name"]] = report_state
-    report_state.append({
-      "parameters": parameters,
-      "timestamp": singer.utils.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-    })
+        records = []
+        for entry in data:
+            record = dict()
+            for property in self.report["properties"]:
+                record[property["name"]] = entry[property["name"]]
+            records.append(record)
+        return records, parameters
 
-    singer.write_state(state)
+    def sync(self, config, state):
+        # time_extracted = singer.utils.now()
+        records, parameters = self.get_records(config)
+        singer.write_records(self.report["name"], records)
+        self._write_state(state, parameters)
 
+    def _write_state(self, state, parameters):
+        report_state = state.get(self.report["name"], [])
+        state[self.report["name"]] = report_state
+        report_state.append({
+            "parameters": parameters,
+            "timestamp": singer.utils.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        })
+
+        singer.write_state(state)
